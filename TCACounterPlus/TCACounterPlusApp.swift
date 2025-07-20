eed2481eea83e99e70c8cd73cb3c6a82c3eea728
Sasha Jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2025-07-20 23:04:49 +0300

    Initial Commit

---
+++ TCACounterPlus/TCACounterPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACounterPlusApp.swift
+//  TCACounterPlus
+//
+//  Created by Sasha Jaroshevskii on 20.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TCACounterPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCACounterPlusTests/TCACounterPlusTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCACounterPlusTests.swift
+//  TCACounterPlusTests
+//
+//  Created by Sasha Jaroshevskii on 20.07.2025.
+//
+
+import Testing
+@testable import TCACounterPlus
+
+struct TCACounterPlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
